b16ddfee6ec4a7d874005c6b76a9514ca4e2af46
diegovaz33 <diegovaz33@gmail.com>
2020-02-02 16:34:15 -0600

    UI

--- test_bookstore/books/views.py
+++ test_bookstore/books/views.py
@@ -1,5 +1,14 @@
-from django.http import HttpResponse
+from django.shortcuts import render
+from . import views
+from django.views import generic, View
 
+
+from django.shortcuts import render, get_object_or_404, redirect
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.db.models import Q
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the books index.")
+    print(request)
+    return render(request, 'index.html')
