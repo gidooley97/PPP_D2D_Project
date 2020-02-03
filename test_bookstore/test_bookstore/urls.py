508aa321abf8685d532c7f91f3e5e5ef20e1794e
BlaiseMahoro <blaise.mahoro@eagles.oc.edu>
2020-02-03 15:20:51 -0600

    set the homepage to be the default

--- test_bookstore/test_bookstore/urls.py
+++ test_bookstore/test_bookstore/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
+    path('', include('books.urls')),
     path('books/', include('books.urls')),
     path('admin/', admin.site.urls),
 ]
