5009bd760ec56c41773add98ded43fafcf6c21d5
gidooley97 <garrett.dooley@eagles.oc.edu>
2020-02-04 17:29:40 -0600

    This commit contains a populated database, a model fix, and a blank detail page.  The model fix allows multiple formats to be selected.

--- test_bookstore/books/models.py
+++ test_bookstore/books/models.py
@@ -1,14 +1,16 @@
 from __future__ import unicode_literals
 from django.db import models
+from django.forms import ModelForm
 
 
-class Book(models.Model):
-    FORMAT_TYPES = (
+FORMAT_TYPES = (
         ('H', 'Hardcover'),
         ('P', 'Paperback'),
         ('E', 'Ebook'),
         ('A', 'Audio')
     )
+
+class Book(models.Model):
     title = models.CharField(max_length=200)
     authors = models.CharField(max_length=200)
     isbn_13 = models.CharField(max_length = 13)
@@ -16,7 +18,7 @@ class Book(models.Model):
     series = models.CharField(max_length = 200,default = '', blank=True)
     volume = models.CharField(max_length = 3, default = '', blank=True)
     desc = models.TextField()
-    book_format = models.CharField(max_length=1, choices=FORMAT_TYPES)
+    book_formats = models.CharField(max_length=4)
     sale_flag = models.BooleanField(default=False)
     
 
@@ -27,3 +29,4 @@ class Book(models.Model):
 class Document(models.Model):
     document = models.FileField(upload_to='documents/')
     uploaded_at = models.DateTimeField(auto_now_add=True)
+
--- test_bookstore/books/urls.py
+++ test_bookstore/books/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('<int:book_id>/', views.detail, name='detail'),
     path('simple_upload', views.simple_upload, name='simple_upload'),
     path('onixfile', views.onixfile, name='onixfile'),
 ]
