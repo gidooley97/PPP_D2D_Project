ea816f6bdc87035153ef37dd98641f33ffd63d5b
BlaiseMahoro <blaise.mahoro@eagles.oc.edu>
2020-02-10 13:44:22 -0600

    Working version for the process_onix API

--- test_bookstore/books/process_onix.py
+++ test_bookstore/books/process_onix.py
@@ -19,15 +19,12 @@ class BookData:
     def __str__(self):
         return self.title +' '+self.isbn_13
 
-def test():
-    bookdata = BookData('some title', 'Blaise Mahoro','12321313112334', 'df', 'df', 'df', 'df', 'df', 'df')
-    #book = Book.objects.create( title='Tiele', authors = 'ldsfklsdfns fsdfn, adfsjdf. dfs',isbn_13= '',subtitle ='',series='',volume='',desc='',book_formats='',sale_flag = '')
-    return bookdata
-
+#Few publishing status
 PUBLISHING_STATUS = {"00": "Unspecified", "01": "Cancelled", "02": "Forthcoming", "03": "Postponed indefinitely", "04": "Active", "05": "No longer our product",
                   "06": "Out of stock indefinitely", "07": "Out of print", "08": "Inactive", "09": "Unknown", "10": "Remaindered",
                   "11": "Withdrawn from sale", "12": "Recalled", "13": "Active, but not solved separately", "15": "Recalled", "16": "Temporarily withdrawn from sale", "17": "Permanently withdraw from sale"}
 
+#few book format codes codes 
 BOOK_FORMATS={"00":"Undefined","AA":"Audio","AB":"Audio cassette","AC":"CD-Audio","AD":"DAT",
  "AE":"Audio disc","AF":"Audio tape","AG":"MiniDisc", "AH":"CD-Extra","AI":"DVD Audio","AJ":"Downloadable audio file",
  "AK":"Pre-recorded digital audio player","BA":"Book","BB":"Hardback","BC":"Paperback / softback",
@@ -48,7 +45,10 @@ def load_onix_file(path):
 def process_data(root):
     #path= "real_stuff_onix3_01.xml"
     #root = load_onix_file(path)
+
     book_list = []
+    if not root: #check if there is no content in the file
+        return book_list
 
     product_elemnts = root.xpath("d:Product", namespaces=ns)
     for prod_el in product_elemnts:
@@ -67,10 +67,14 @@ def process_data(root):
         if prod_el.xpath(".//d:DescriptiveDetail/d:TitleDetail[d:TitleType='01']/d:TitleElement/d:Subtitle", namespaces=ns):
             subtitle=prod_el.xpath(".//d:DescriptiveDetail/d:TitleDetail[d:TitleType='01']/d:TitleElement/d:Subtitle", namespaces=ns)[0].text
         collection_el=prod_el.xpath(".//d:Collection", namespaces=ns)
-        volume = ''
+        volume = '-'
         if collection_el:
             if prod_el.xpath(".//d:PartNumber", namespaces=ns):
                 volume = prod_el.xpath(".//d:PartNumber", namespaces=ns)[0].text
+        series ='None'
+        if prod_el.xpath(".//d:Collection/d:TitleDetail/d:TitleElement[d:SequenceNumber='2']/d:TitleText", namespaces=ns):
+            series = prod_el.xpath(".//d:Collection/d:TitleDetail/d:TitleElement[d:SequenceNumber='2']/d:TitleText", namespaces=ns)[0].text
+
         description = prod_el.xpath(".//d:TextContent[d:TextType='03']/d:Text", namespaces=ns)[0].text
         pub_stat_code=prod_el.xpath(".//d:PublishingStatus", namespaces=ns)[0].text
         publ_status =PUBLISHING_STATUS[pub_stat_code]
@@ -79,11 +83,10 @@ def process_data(root):
         sale_flag= (pub_stat_code=="13" or pub_stat_code=="04")
         #data
         auths= ','.join(authors)
-        series =''
+        
         
         tmp_book_data = BookData(title,auths,isbn_13,subtitle,series,volume ,description, book_format,sale_flag)
         book_list.append(tmp_book_data) #Add book data to list
     return book_list
 
 
-#process_data()
