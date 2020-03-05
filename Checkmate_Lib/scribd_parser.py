77994e01bee750adb845898c3c7cb9e062d9721a
JeremyMarz <32718560+JeremyMarz@users.noreply.github.com>
2020-03-04 22:30:07 -0600

    Scribd Parser function update
    -adds site_book_data functionality
    -matching function
    -get_book_data

--- Checkmate_Lib/scribd_parser.py
+++ Checkmate_Lib/scribd_parser.py
@@ -6,13 +6,17 @@ from PIL import Image
 import requests
 from io import BytesIO
 import urllib.request
-import re
+from bookSite import BookSite
+import mechanize
 
 ############ Scribd Site Class ################
 """parses the book data from Scribd"""
 class ScribdSite:
     def __init__(self):
-        pass
+        self.site_slug = "SC"
+        self.search_url = "https://www.scribd.com"
+        self.url_to_book_detail ="https://www.scribd.com/book"#set to ebook bcs D2D deals with ebooks mainly
+        self.match_list=[]
      
     def get_book_data_from_site(self,url):
         content = requests.get(url).content#gets the book's page 
@@ -41,30 +45,74 @@ class ScribdSite:
         ready_for_sale, extra)
         return book_site_data
 
-    def find_matches_at_site(self,book_data):
-        pass
+    def find_matches_at_site(self,site_book_data):
+        search_txt =''
+        if site_book_data.book_title:
+            search_txt=site_book_data.book_title
+        elif site_book_data.isbn13:
+            search_txt= site_book_data.isbn_13
+        elif site_book_data.authors:
+            search_txt = site_book_data.authors[0]
+        if not search_txt:
+            return []
+
+        payload ={'ft':search_txt, 'originalText':search_txt}
+        content = requests.get(self.search_url,params=payload).content
+        url =  requests.get(self.search_url,params=payload).url
+        self.__get_book_data_from_page(content,site_book_data)
+        page=1
+        while(True):
+            print('Page',page)
+            content= requests.get(url+"#"+str(page)).content
+
+            parser = etree.HTMLParser(remove_pis=True)
+            tree = etree.parse(io.BytesIO(content), parser)
+            root = tree.getroot()
+          
+            self.__get_book_data_from_page(content, site_book_data)
+            if root.xpath(".//nav[@aria-label='Pagination']"):
+                if root.xpath(".//nav[@aria-label='Pagination']/ul/li[6]/a"):
+                    print("done!")
+                    break
+                else:
+                    page+=1
+            else:
+                print("done!")
+                break
+        
+        return self.match_list
+
+    def __get_book_data_from_page(self, content, book_site_dat_1):
+        parser = etree.HTMLParser(remove_pis=True)
+        tree = etree.parse(io.BytesIO(content), parser)
+        root = tree.getroot()
+        url_elements = root.xpath(".//div[@class='list_anchor_container']/a/@href")
+
+        for url in url_elements:
+            #call function to get book data with url
+            #print('url', url)
+            book_site_dat_tmp= self.get_book_data_from_site(url)
+            score = self.match_percentage(book_site_dat_1, book_site_dat_tmp) 
+            book_data_score =tuple([score,book_site_dat_tmp])
+            self.match_list.append(book_data_score)
 
     def convert_book_id_to_url(self,book_id):
         url = "https://www.scribd.com/book/"+book_id
         return url
 
+    def match_percentage(self, site_book1, site_book2):
+        return super().match_percentage(site_book1,site_book2)
+
     #------------ Utility Methods -------------
-    def titleParser(self, content):
-        parser = etree.HTMLParser(remove_pis=True)
-        tree = etree.parse(io.BytesIO(content), parser)
-        root = tree.getroot()
+    def titleParser(self, root):
         title_element = root.xpath(".//h1[@class='document_title']")[0]
         title = title_element.text
-        print(title)
         return title
 
-    def subtitleParser(self,content):
+    def subtitleParser(self,root):
         pass
 
-    def authorsParser(self,content):
-        parser = etree.HTMLParser(remove_pis=True)
-        tree = etree.parse(io.BytesIO(content), parser)
-        root = tree.getroot()
+    def authorsParser(self,root):
         author_elements = root.xpath(".//a[contains(@href,'https://www.scribd.com/author')]")
         print(author_elements)
         authors = []
@@ -73,20 +121,18 @@ class ScribdSite:
             print(auth_element.text)
         return authors
 
-    def isbnParser(self, content):
-        parser = etree.HTMLParser(remove_pis=True)
-        tree = etree.parse(io.BytesIO(content), parser)
-        root = tree.getroot() 
+    def isbnParser(self, root):
         isbn_element = root.xpath("/html/head/meta[18]/@content")
         isbn = isbn_element[0]
         print(isbn)
         return isbn
 
+    def book_id_parser(self, url): 
+        book_id = url.split('/')[len(url.split('/'))-2]
+        print(book_id)
+        return book_id
 
-    def formatParser(self, content):
-        parser = etree.HTMLParser(remove_pis=True)
-        tree = etree.parse(io.BytesIO(content), parser)
-        root = tree.getroot() 
+    def formatParser(self, root):
         format_element = root.xpath("/html/head/meta[13]/@content")
         form = format_element[0]
         print(form)
@@ -101,14 +147,11 @@ class ScribdSite:
             print("error")
         return image
 
-    def descParser(self, content):
-        parser = etree.HTMLParser(remove_pis=True)
-        tree = etree.parse(io.BytesIO(content), parser)
-        root = tree.getroot() 
+    def descParser(self, root):
         desc_elements = root.xpath("/html/head/meta[16]/@content")
-        desc = desc_elements[0]
+        desc = desc_elements
         print(desc) 
-        return desc
+        return str(desc)
 
     def seriesParser(self, title):
         title = ""
@@ -144,40 +187,40 @@ class ScribdSite:
         print(volume)
         return volume
 
-    def saleReadyParser(self, content):
+    def saleReadyParser(self, root):
         saleReady = "Avaliable"
         print(saleReady)
         return saleReady
     
-    def extraParser(content):
+    def extraParser(self,root):
         pass
 
-    def imageUrlParser(self, content):
-        parser = etree.HTMLParser(remove_pis=True)
-        tree = etree.parse(io.BytesIO(content), parser)
-        root = tree.getroot() 
+    def imageUrlParser(self, root):
         imageUrlParser_element = root.xpath("/html/head/link[5]/@href")
         imageURL = imageUrlParser_element[0]
         print(imageURL) 
         return imageURL
 
-    #parseAll parses all data, prints it, and 
-    #stores it in a SiteBookData Object
-    def parseAll(content, SiteBookData):
-        pass
+    def get_parse_status(self,title, isbn13, desc, authors):
+        #determine parse_status checks if we have the most basic data about a book
+        if title and isbn13 and desc and authors:
+            return "UNSUCCESSFUL"
+        return "FULLY_PARSED"
 
-    ############# End of Class #################
-
+  ############# End of Class #################
 
 
 def main():
-    url = "https://www.scribd.com/book/163638327/Good-Omens-The-Nice-and-Accurate-Prophecies-of-Agnes-Nutter-Witch"
-    #url = prompt("Enter a url");
+    url = "https://www.scribd.com/book/163638327"
     content = fetch(url)
     site = ScribdSite() 
-    title = site.titleParser(content)
-    site.authorsParser(content)
-
+    
+    book = site.get_book_data_from_site(url)
+    matches = site.find_matches_at_site(book)
+    for x in matches:
+        x.print_all()
+        
+    
 
   
 def fetch(url):
