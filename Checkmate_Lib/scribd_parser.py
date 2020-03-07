01ecda8f3ec5ab7d9f8eaa3a3b9dad012d221d1e
BlaiseMahoro <blaise.mahoro@eagles.oc.edu>
2020-03-07 10:58:33 -0600

    added feature to get multiple pages

--- Checkmate_Lib/scribd_parser.py
+++ Checkmate_Lib/scribd_parser.py
@@ -14,7 +14,7 @@ import json
 class ScribdSite(BookSite):
     def __init__(self):
         self.site_slug = "SC"
-        self.search_url = "https://www.scribd.com/search?content_type=books&page=1&query=&language=1"
+        self.search_url = "https://www.scribd.com/search"
         self.url_to_book_detail ="https://www.scribd.com/book"#set to ebook bcs D2D deals with ebooks mainly
         self.match_list=[]
      
@@ -45,7 +45,8 @@ class ScribdSite(BookSite):
         ready_for_sale=ready_for_sale, extra=extra)
         return book_site_data
 
-    def find_matches_at_site(self,site_book_data):
+    def find_matches_at_site(self,site_book_data, pages = 2):
+        self.match_list=[]
         search_txt =''
         if site_book_data.book_title:
             search_txt=site_book_data.book_title
@@ -55,22 +56,33 @@ class ScribdSite(BookSite):
             search_txt = site_book_data.authors[0]
         if not search_txt:
             return []
-        url =self.search_url
         br = mechanize.Browser()
         br.set_handle_robots(False)
         br.addheaders = [('User-agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1')]
-        br.open(url)  
-        br.select_form(class_="search_form")#selects form
-        br['query']=search_txt #fills the input field
-        print(br['query'])
-        res=br.submit()
-        content=res.read()
-        urls=self.get_urls_js(content)
-        #print(content)
+        page =1
+        url=self.search_url+'?content_type=books&page=1&language=1&query='+search_txt
+        content = br.open(url).read() 
+        page_count = self.get_page_count_from_js(content)#gets page count
+        while page <= pages and page <= page_count:  #for testing we want to get a few pages
+            payload ={'content_type':'books', 'language':'1', 'page':page, 'query':search_txt}
+            content = requests.get(self.search_url,params=payload).content
+            urls=self.get_urls_js(content)
+            page+=1
         for r in urls:
             print(r)#just fo debugging
         self.__get_book_data_from_page(urls,site_book_data)        
         return self.match_list
+
+    #parse the javascript code and gets the page count
+    def get_page_count_from_js(self, content):
+        parser = etree.HTMLParser(remove_pis=True)
+        tree = etree.parse(io.BytesIO(content), parser)
+        root = tree.getroot()
+        dom_txt = root.xpath(".//script")[21].text
+        tmp_txt = dom_txt.split('"page_count":')[1].split(',')[0]
+        page_count=int(tmp_txt)
+        print('pages',page_count)
+        return page_count
     #gets the resuts as a json from the javascript of the results page 
     #and returns a list of direct urls to books 
     def get_urls_js(self,content):
@@ -212,14 +224,14 @@ class ScribdSite(BookSite):
 
 
 def main():
-    url = "https://www.scribd.com/book/295105408/The-MacArthur-Study-Bible"
+    url = "https://www.scribd.com/book/190969296/Lord-of-the-Flies"
     content = fetch(url)
     site = ScribdSite() 
     
     book = site.get_book_data_from_site(url)
     matches = site.find_matches_at_site(book)
-    for x in matches:
-        x[1].print_all()
+    # for x in matches:
+    #     x[1].print_all()
         
     
 
