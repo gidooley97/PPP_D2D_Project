90101dcf50aaa8ade563edeb074abb6c0a1ae8ca
BlaiseMahoro <blaise.mahoro@eagles.oc.edu>
2020-03-11 22:50:04 -0500

    Fixing the findMatches function

--- Checkmate_Lib/google_books_parser.py
+++ Checkmate_Lib/google_books_parser.py
@@ -9,12 +9,13 @@ import urllib.request
 from bookSite import BookSite
 import mechanize
 import checkmate
+from bookSite import BookSite
 import re
 
-class GoogleBooks:
+class GoogleBooks(BookSite):
     def __init__(self):
         self.site_slug = "GB"
-        self.search_url = "https://www.books.google.com/"
+        self.search_url = "https://www.google.com/search?q=&source=lnms&tbm=bks&sa=X&ved=2ahUKEwj10bG89ZDoAhUEnKwKHcC4B-YQ_AUoAXoECBQQCQ&biw=1235&bih=634"
         self.url_to_book_detail = "https://www.books.google.com/book?vid=ISBN"
         self.match_list = []
 
@@ -39,7 +40,32 @@ class GoogleBooks:
         ready_for_sale = self.sales_flag_parser(root)
         extra = self.extraParser(root)
         book_site_data = SiteBookData(format=frmt, book_title=title, book_img= img, book_img_url=img_url, isbn_13=isbn13, description=desc, series=series, 
-        volume=None, subtitle=subtitle, authors=authors, book_id=None, site_slug=site_slug, parse_status=parse_status, url=url, content=content,
+        volume="", subtitle=subtitle, authors=authors, book_id=None, site_slug=site_slug, parse_status=parse_status, url=url, content=content,
+        ready_for_sale=ready_for_sale, extra=extra)
+        return book_site_data
+    #for testing
+    def get_book_data_from_site_v2(self, content):
+        #content = requests.get(url).content
+        parser = etree.HTMLParser(remove_pis=True)
+        tree = etree.parse(io.BytesIO(content), parser)
+        root = tree.getroot()
+        frmt = self.format_parser(root)
+        title = self.titleParser(root)
+        img_url = self.imageUrlParser(root)
+        img = self.imageParser(img_url)
+        isbn13 = self.isbnParser(root)
+        desc = self.descriptionParser(root)
+        series = self.seriesParser(root)
+        subtitle = self.subtitleParser(root)
+        authors = self.authorsParser(root)
+        site_slug = self.site_slug
+        content = content
+        #url = url
+        parse_status = self.get_parse_status(title, isbn13, desc, authors)
+        ready_for_sale = self.sales_flag_parser(root)
+        extra = self.extraParser(root)
+        book_site_data = SiteBookData(format=frmt, book_title=title, book_img= img, book_img_url=img_url, isbn_13=isbn13, description=desc, series=series, 
+        volume="", subtitle=subtitle, authors=authors, book_id=None, site_slug=site_slug, parse_status=parse_status, url=None, content=content,
         ready_for_sale=ready_for_sale, extra=extra)
         return book_site_data
 
@@ -47,8 +73,13 @@ class GoogleBooks:
         url = self.search_url
         br = mechanize.Browser()
         br.set_handle_robots(False)
-        br.open(url)
-        br.select_form(id_="oc-search-input")
+        br.addheaders = [('User-agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1')]
+        res= br.open(url)
+
+        #print(res.read())
+        
+        br.select_form(name="f")
+        #print('form', br)
         search_txt = ''
         if site_book_data.book_title:
             search_txt=site_book_data.book_title
@@ -58,31 +89,64 @@ class GoogleBooks:
             search_txt = site_book_data.authors[0]
         if not search_txt:
             return []
-        br['query'] = search_txt
+        br['q'] = search_txt
 
         res = br.submit()
-        self.__get_book_data_from_page(res.read(), site_book_data)
+        fileobj = open("page1.html","wb")#saves the returned page to a file. 
+        #You can open and se the content
+        content = res.read()
+        fileobj.write(content)
+        fileobj.close()
+        self.__get_book_data_from_page(content,br, site_book_data)
         return self.match_list
         while(True):
             try:
                 print("nextpage")
                 res = br.follow_link(text="Next")
-                self.__get_book_data_from_page(res.read(), site_book_data)
+                self.__get_book_data_from_page(res.read(), br, site_book_data)
             except mechanize._mechanize.LinkNotFoundError:
                 print("Reached end of results")
                 return self.match_list
 
-    def __get_book_data_from_page(self, content, book_site_dat_1):
+    def __get_book_data_from_page(self, content, br, book_site_dat_1):
+        #print(content)
         parser = etree.HTMLParser(remove_pis=True)
         tree = etree.parse(io.BytesIO(content), parser)
         root = tree.getroot()
-        url_elements = root.xpath(".//div[@class='bHexk']/a/@href")
-
+        url_elements = root.xpath('//a[@class="fuLhoc ZWRArf"]/@href')
+        print("urls", len(url_elements))
         for url in url_elements:
-            book_site_dat_temp = self.get_book_data_from_site(url)
+            print('url',url)
+            #book_site_dat_temp = self.get_book_data_from_site(url)
+
+            book_site_dat_temp=self.navigate_to_view_ebook_page(br, url)
+            if not book_site_dat_temp:
+                continue
             score = self.match_percentage(book_site_dat_1, book_site_dat_temp)
             book_data_score = tuple([score,book_site_dat_temp])
             self.match_list.append(book_data_score)
+
+    def navigate_to_view_ebook_page(self,br, url):
+        content=br.open(url).read()
+        parser = etree.HTMLParser(remove_pis=True)
+        tree = etree.parse(io.BytesIO(content), parser)
+        root = tree.getroot()
+        if root.xpath('//p[@id="gb-get-book-not-available"]'):
+            return 
+        try:
+            res=br.follow_link(text="View eBook")
+        except mechanize._mechanize.LinkNotFoundError:
+            try:
+                return self.get_book_data_from_site(url)
+            except:
+                return
+        fileobj = open("page1.html","wb")#saves the returned page to a file. 
+        #You can open and se the content
+        content = res.read()
+        fileobj.write(content)
+        fileobj.close()
+        return self.get_book_data_from_site_v2(content)
+        
 
     def convert_book_id_to_url(self, book_id):
         return self.url_to_book_detail+book_id
