e94dbbce7a23368cda2869a2c4b2bceb8b461455
gidooley97 <garrett.dooley@eagles.oc.edu>
2020-03-28 20:14:15 -0500

    Constructed Model

--- B2B_site/clients/models.py
+++ B2B_site/clients/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
 
-# Create your models here.
+
+class User(AbstractBaseUser):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length = 50)
+    email = models.EmailField(max_length=70,unique=True)
+    num_queries = models.IntegerField() 
+
+    def __str__(self):              
+        return self.first_name + self.last_name
+    
+    def get_email(self):
+        return self.email
+    
+    
