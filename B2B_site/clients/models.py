3420c0b0c47656a6cddb7a4d1fa5eeb81beb9a8f
gidooley97 <garrett.dooley@eagles.oc.edu>
2020-03-31 13:56:51 -0500

    Working User

--- B2B_site/clients/models.py
+++ B2B_site/clients/models.py
@@ -1,17 +1,24 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.utils.translation import ugettext_lazy as _
 
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    num_queries = models.IntegerField(default = 0)
 
-class User(AbstractBaseUser):
-    first_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length = 50)
-    email = models.EmailField(max_length=70,unique=True)
-    num_queries = models.IntegerField() 
+    def get_num_queries():
+        return num_queries
+    
+    def inc_num_queries():
+        num_queries += 1
 
-    def __str__(self):              
-        return self.first_name + self.last_name
-    
-    def get_email(self):
-        return self.email
-    
-    
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
