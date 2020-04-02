03e7749a98f158bfa4849cff4775ad87e40fe522
gidooley97 <garrett.dooley@eagles.oc.edu>
2020-04-01 20:17:59 -0500

    Restructured Model to handle dates and populated DB

--- B2B_site/clients/models.py
+++ B2B_site/clients/models.py
@@ -6,13 +6,30 @@ from django.utils.translation import ugettext_lazy as _
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    num_queries = models.IntegerField(default = 0)
+
+    def query_handler(self, date):        
+        q_set =  Query_Manager.objects.all()
+        for q in q_set:
+            if date.date == q.get_date().date:
+                q.inc_num_queries
+            else:
+                new_qm = Query_Manager(last_date = date)
+                new_qm.inc_num_queries
+
+
+class Query_Manager(models.Model):
+    user = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    num_queries = models.IntegerField(default =0)
+    last_date = models.DateField()
 
     def get_num_queries():
         return num_queries
     
     def inc_num_queries():
         num_queries += 1
+    
+    def get_date():
+        return last_date
 
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
