874565fba1730d92bcfcd8a58f662fcd4fa97a58
Diego Vazquez <diegovaz33@gmail.com>
2020-04-05 16:23:24 -0500

    Revert "Revert "Merge branch 'model_for_b2b' of https://github.com/gidooley97/PPP_TestBookStore_v2 into model_for_b2b""
    This reverts commit b88afbf85c6116d561b4a5491aeda9b3940e5527.

--- Checkmate_Lib/checkmate.py
+++ Checkmate_Lib/checkmate.py
@@ -3,25 +3,33 @@
 from parsers.kobo_parser import KoboSite
 from parsers.livraria_parser import LivrariaSite
 from parsers.google_books_parser import GoogleBooks
-from parsers.test_parser import TestSite
+from parsers.test_book_store_parser import TestSite
 from parsers.scribd_parser import ScribdSite
+from parsers.audiobook_parser import AudioBookSite
 
+"""
+returns site parser.
 
+params:
+    slug: 2 character slug indicating the site
+return:
+    site: a BookSite object matching the passed slug.
+"""
 def get_book_site(slug_cased):
     site  = None
     slug = slug_cased.upper()
-    if slug == 'GB':
+    if slug == GoogleBooks().site_slug:
         site = GoogleBooks()
-    elif slug == 'KO':
+    elif slug == KoboSite().site_slug:
         site = KoboSite() 
-    elif slug == 'TB':
+    elif slug == TestSite().site_slug:
         site = TestSite()
-    elif slug == 'LC':
+    elif slug == LivrariaSite().site_slug:
         site = LivrariaSite()  
-    elif slug == 'SC':
+    elif slug == ScribdSite().site_slug:
         site = ScribdSite()
-        
-
+    elif slug == AudioBookSite().site_slug:
+        site = AudioBookSite()
     return site
 
    
