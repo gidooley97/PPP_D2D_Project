5ceadf30d89065aa33e5046d67caa880e5a668d9
Diego Vazquez <diegovaz33@gmail.com>
2020-04-04 19:44:33 -0500

    Login/logout

--- B2B_site/clients/urls.py
+++ B2B_site/clients/urls.py
@@ -5,4 +5,8 @@ from . import views
 urlpatterns = [
     path('', views.SearchResultsView.as_view(), name='search'),
     path('search/', views.SearchResultsView.as_view(), name='search'),
+    path('logout/', views.logoutView.as_view(), name='logged_out'),
+    path('login/', views.loginView.as_view(), name='login'),
+
+
 ]
