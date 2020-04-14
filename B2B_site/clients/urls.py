5fd96338fb0c7b750de069b32a2a2023699bf52f
BlaiseMahoro <blaise.mahoro@eagles.oc.edu>
2020-04-14 11:00:48 -0500

    Added APi auth endpoint

--- B2B_site/clients/urls.py
+++ B2B_site/clients/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-
+from rest_framework.authtoken.views import obtain_auth_token  # <-- Here
 from . import views
 
 
@@ -11,6 +11,6 @@ urlpatterns = [
     # path('logout/', views.LogoutView.as_view(), name='logged_out'),
     # path('login/', views.LoginView.as_view(), name='login'), #No need for these
     path('activity/', views.activity, name='activity'),
-    #path('api-token-auth/', views.get_auth_token, name='api-token-auth'),
+    path('api-token-auth/', obtain_auth_token, name='api_token_auth'), #to get the auth token send a POST request http://127.0.0.1:8000/clients/api-token-auth/ with the body{ username:<username>, password:<pass>}
     
 ]
