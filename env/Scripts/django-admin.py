61b6050b301c7f0dcd935f4307b15439bd0c09e0
gidooley97 <48361104+gidooley97@users.noreply.github.com>
2020-04-16 13:09:17 -0500

    yes

---
+++ env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\admin\Documents\SE V\Cap Stone Project\project\ppp_proj\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
